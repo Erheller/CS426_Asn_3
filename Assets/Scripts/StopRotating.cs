13e0c35be3da53fb2bc9c962a3b12ed24402484e
Erheller <kdwawesome@gmail.com>
2017-02-08 20:45:41 -0600

    Added some prefabs and stuff

---
+++ Assets/Scripts/StopRotating.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopRotating : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, lockPos, lockPos);
+		transform.rotation = Quaternion.Euler(-90, transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.x);
+	}
+}
