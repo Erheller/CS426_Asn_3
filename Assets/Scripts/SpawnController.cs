f0e804fd51244d7a7fe84fb761865c1cec9be040
Daniel Pulley <dpulley@uic.edu>
2017-02-08 19:55:01 -0800

    Create spawnController script to control the spawning of joes

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+
+	public GameObject joe; // the joe prefab
+	public float spawnTime = 3f; // time to wait before spawning
+	public Transform[] spawnPoints;	// An array of the spawn points this enemy can spawn from.
+	private int joeCounter = 0;
+	private int joeMax = 7;
+
+	// Spawn Joe
+	void Spawn()
+	{
+		// Find a random index between zero and one less than number of spawn points.
+		int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+		// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+		Instantiate(joe, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+		joeCounter++;
+		if (joeCounter >= joeMax)
+			CancelInvoke ("Spawn");
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		// Call the spawn after a delay of the spawnTime and then continue to call after the same amount of time.
+		InvokeRepeating("Spawn", spawnTime,spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
